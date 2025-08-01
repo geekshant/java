62bfbdf8ba7426b0bdff9c5f4cc1657861f0eef8
deekshant <deekshant72823@gmail.com>
2025-08-02 03:32:47 +0530

    24 push

---
+++ Stack.java
@@ -0,0 +1,59 @@
+
+public class Stack {
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Stac {
+
+        private Node head;
+
+        public boolean isEmpty() {
+            return head == null;
+        }
+
+        public void push(int data) {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stac s = new Stac();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
